67751af793800178a684c55c7ad38aa5e436dd5a
Leo Adamek <leo.adamek@mrzen.com>
2023-01-31 10:59:19 +0000

    Authentication WIP

--- src/modules/fields.ts
+++ src/modules/fields.ts
@@ -1,24 +1,45 @@
 import {ApiGroup} from "./common";
 
+/**
+ * User-defined field data
+ *
+ * Data for user-defined fields are stored as a mapping of field names to a value datum
+ */
 export type FieldData = { [name: string]: FieldValue }
 
+/**
+ * User-defined field value datum
+ *
+ */
 export type FieldValue = StringFieldValue | NumberFieldValue | BooleanFieldValue | SelectionFieldValue
 
+/**
+ * Datum for a field containing string data
+ */
 export interface StringFieldValue {
     type: 'string' | 'rich_text' | 'url' | 'color' | 'date' | 'date_time'
     value: string
 }
 
+/**
+ * Datum for a field containing numeric data
+ */
 export interface NumberFieldValue {
     type: 'number'
     value: number
 }
 
+/**
+ * Datum for a field containing boolean data
+ */
 export interface BooleanFieldValue {
     type: 'boolean'
     value: boolean
 }
 
+/**
+ * Datum for a field containing enum values
+ */
 export interface SelectionFieldValue {
     type: 'select'
     value: string[]
