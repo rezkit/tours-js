e73b5d9b5498b7e1e228107692ee4a5fd482d5d5
Tom Hood <tom.hood@mrzen.com>
2023-04-27 15:14:22 +0100

    Add missing type on Element.

--- src/modules/elements.ts
+++ src/modules/elements.ts
@@ -5,8 +5,10 @@ import {ApiGroup} from "./common.js";
 import timestamp from "../annotations/timestamp.js";
 
 export interface IElement extends Entity {
-
+    name: string
+    
     version_id: string
+    
     is_package: boolean
 
     published: boolean
