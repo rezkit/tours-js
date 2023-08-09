a989f7d586a189945448d6ed7c192681f82214a5
S4-K1 <hardcastle.riley@gmail.com>
2023-08-09 09:52:57 +0100

    Remove unused

--- src/modules/organizations.ts
+++ src/modules/organizations.ts
@@ -1,21 +1,13 @@
 import type {
     Entity,
-    EntityType,
     Paginated,
     PaginatedQuery,
-    QueryBoolean,
     SortableQuery,
-    ID,
     Fields
 } from "./common.js";
 import type {AxiosInstance} from "axios";
 import {ApiGroup} from "./common.js";
 import type {TreeNode} from "../helpers";
-import timestamp from "../annotations/timestamp.js";
-import type { FieldData } from "./fields";
-import {Category, CategoryAttachment, type ICategory} from "./categories";
-import type {IHoliday} from "./holidays";
-import {Holiday} from "./holidays";
 
 export interface IOrganization extends Entity, TreeNode, Fields {
     id: string;
@@ -46,10 +38,5 @@ export class Api extends ApiGroup {
         response.data = response.data.map(o => new Organization(o, this.axios))
 
         return response as Paginated<Organization>
-    }
-
-    async find(id: string): Promise<Organization> {
-        const o = (await this.axios.get<IOrganization>(`/organizations/${id}`)).data
-        return new Organization(o, this.axios)
     }
 }
