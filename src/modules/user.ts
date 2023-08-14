a231f44c0ea45a5f0079a4b63779e8386a69d272
S4-K1 <hardcastle.riley@gmail.com>
2023-08-14 11:43:45 +0100

    Declare formats in API

--- src/modules/user.ts
+++ src/modules/user.ts
@@ -56,6 +56,7 @@ export interface OrganizationSettings {
         balance_due: number
         percentage: number
     }
+    image_formats: Object[]
 }
 
 export type UpdateOrganizationSettings = Partial<OrganizationSettings>;
