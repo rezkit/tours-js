c29bc0a021544e5e1d7830d3a5c7f81a53db5a6f
Leo Adamek <leo.adamek@mrzen.com>
2023-09-28 11:32:06 +0100

    Additional ESLint fixes.

--- src/errors.ts
+++ src/errors.ts
@@ -1,23 +1,21 @@
-export class ValidationError implements Error {
-  message: string
+export class ValidationError extends Error {
   public name: string = 'Validation Error'
 
   errors: Record<string, string[]>
 
   constructor ({ message, errors }: { message: string, errors: Record<string, string[]> }) {
-    this.message = message
+    super(message)
     this.errors = errors
   }
 }
 
-export class NotFoundError implements Error {
-  message: string
+export class NotFoundError extends Error {
   name: string = 'Not Found'
 
   readonly uri: string
 
   constructor (message: string, uri: string) {
-    this.message = message
+    super(message)
     this.uri = uri
   }
 }
