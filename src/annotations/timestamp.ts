e5e05542943f0b1f25b9e8f8212f9de3b5806d76
Tom Hood <tom.hood@mrzen.com>
2025-03-18 11:57:43 +0000

    Dependency upgrade review and testing.

--- src/annotations/timestamp.ts
+++ src/annotations/timestamp.ts
@@ -1,23 +1,26 @@
 /**
  * Annotation which will always return the given property as a `Date`
  */
-export default function timestamp () {
+export default function timestamp() {
   return (target: any, name: PropertyKey) => {
-    // Internal name of the property to actually store the value against
-    const internalName = Symbol.for('__timestamp_' + String(name))
+    if (typeof target !== 'object' || target === null) {
+      throw new Error('Target must be an object');
+    }
+
+    const internalName = Symbol.for('__timestamp_' + String(name));
 
     Object.defineProperty(target, name, {
-      get (this: any) {
+      get(this: any) {
         if (typeof this[internalName] === 'string') {
-          return new Date(this[internalName])
+          return new Date(this[internalName]);
         }
 
-        return this[internalName]
+        return this[internalName];
       },
 
-      set (this: any, value: Date | string) {
-        this[internalName] = value
+      set(this: any, value: Date | string) {
+        this[internalName] = value;
       }
-    })
-  }
+    });
+  };
 }
