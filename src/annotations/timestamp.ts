f598d268e21f150c707464354854703c150f7e82
Tom Hood <tom.hood@mrzen.com>
2025-03-18 12:03:03 +0000

    Dependency review and testing. Log target.

--- src/annotations/timestamp.ts
+++ src/annotations/timestamp.ts
@@ -3,22 +3,23 @@
  */
 export default function timestamp() {
   return (target: any, name: PropertyKey) => {
-    if (typeof target !== 'object' || target === null) {
-      throw new Error('Target must be an object');
+    console.log(target);
+    
+    if (typeof target === 'function' && target.prototype) {
+    } else if (typeof target !== "object" || target === null) {
+      throw new Error("Target must be an object");
     }
 
-    const internalName = Symbol.for('__timestamp_' + String(name));
+    const internalName = Symbol.for("__timestamp_" + String(name));
 
     Object.defineProperty(target, name, {
-      get(this: any) {
-        if (typeof this[internalName] === 'string') {
+      get() {
+        if (typeof this[internalName] === "string") {
           return new Date(this[internalName]);
         }
-
         return this[internalName];
       },
-
-      set(this: any, value: Date | string) {
+      set(value) {
         this[internalName] = value;
       }
     });
