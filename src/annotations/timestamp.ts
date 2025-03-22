d34f607c913f90312c23f9bc82011eca1a2f1a31
Tom Hood <tom.hood@mrzen.com>
2025-03-22 01:44:54 +0000

    Prototype approach.

--- src/annotations/timestamp.ts
+++ src/annotations/timestamp.ts
@@ -2,7 +2,7 @@
  * Annotation which will always return the given property as a `Date`
  */
 export default function timestamp() {
-  return function (target: any, propertyKey: string | symbol): void {
+  return function (target: any, propertyKey: string | symbol) {
     const internalKey = Symbol(propertyKey.toString());
 
     Object.defineProperty(target, propertyKey, {
@@ -10,11 +10,12 @@ export default function timestamp() {
         return this[internalKey] ? new Date(this[internalKey]) : undefined;
       },
       set(value) {
-        this[internalKey] = value instanceof Date ? value.toISOString() : value;
+        this[internalKey] = value ? new Date(value).toISOString() : undefined;
       },
       enumerable: true,
       configurable: true,
     });
+    
+    target.constructor.prototype[propertyKey] = undefined;
   };
 }
-
